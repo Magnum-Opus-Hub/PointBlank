86f4bc9e20ec392088c874d60579aab005a6a1f0
FilipTheGod <filipstan666@gmail.com>
2023-08-25 00:58:45 +0300

    wow

---
+++ app/page.js
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <main className="container">
+      <div className="row">
+      <h1>Next.js Image Component</h1>
+      </div>
+      </main>
+
+  )
+}
