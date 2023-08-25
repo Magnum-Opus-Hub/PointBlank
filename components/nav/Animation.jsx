9a1fe9ae1f2be8b1c40a3d714c9b8e6f4249e0f4
FilipTheGod <filipstan666@gmail.com>
2023-08-25 20:37:41 +0300

    iphone video

--- components/nav/Animation.jsx
+++ components/nav/Animation.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 const Animation = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <video autoPlay muted loop className="absolute inset-0 w-full h-full object-cover absolute-video">
-        <source src='/pbklogo.mp4' type='video/mp4' />
-        Your browser does not support the video tag
-      </video>
+     <video autoPlay muted loop playsInline className="absolute inset-0 w-full h-full object-cover">
+  <source src='/pbklogo.mp4' type='video/mp4' />
+  Your browser does not support the video tag
+</video>
+
     </div>
   );
 };
