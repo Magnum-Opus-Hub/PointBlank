0382b802b0870c0544bd500806babd63c21d4441
FilipTheGod <filipstan666@gmail.com>
2023-08-25 20:28:20 +0300

    video

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
-
+import React from "react"
+import Animation from "@/components/nav/Animation"
 
 export default function Home() {
   return (
-    <main className="container">
-      <div className="row">
-      <h1>Next.js Image Component</h1>
-      </div>
-      </main>
+    <div >
+      <Animation />
+    </div>
 
   )
 }
---
+++ components/nav/Animation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Animation = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <video autoPlay muted loop className="absolute inset-0 w-full h-full object-cover absolute-video">
+        <source src='/pbklogo.mp4' type='video/mp4' />
+        Your browser does not support the video tag
+      </video>
+    </div>
+  );
+};
+
+export default Animation;
