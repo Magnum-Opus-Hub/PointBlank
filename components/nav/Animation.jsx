ce04f5be705e4337f6a45965b3819068b609853e
FilipTheGod <filipstan666@gmail.com>
2023-08-25 20:43:16 +0300

    iphone video 3

--- components/nav/Animation.jsx
+++ components/nav/Animation.jsx
@@ -1,26 +1,27 @@
 'use client'
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 
 const Animation = () => {
   const [isVideoPlaying, setVideoPlaying] = useState(false);
 
-  const handlePlayClick = () => {
+  useEffect(() => {
     setVideoPlaying(true);
-  };
+  }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      {isVideoPlaying ? (
-        <video autoPlay muted loop playsInline className="absolute inset-0 w-full h-full object-cover">
+    <div className="flex items-center justify-center h-screen relative">
+      {isVideoPlaying && (
+        <video autoPlay muted loop playsInline className="w-full h-full object-cover">
           <source src='/pbklogo.mp4' type='video/mp4' />
           Your browser does not support the video tag
         </video>
-      ) : (
-        <div className="relative">
-          <button onClick={handlePlayClick} className="absolute inset-0 w-full h-full bg-black bg-opacity-50 text-white flex items-center justify-center">
-            Play Video
-          </button>
-        </div>
+      )}
+
+      {!isVideoPlaying && (
+        <button onClick={() => setVideoPlaying(true)} className="absolute inset-0 w-full h-full bg-black bg-opacity-50 text-white flex items-center justify-center">
+          Play Video
+        </button>
       )}
     </div>
   );
