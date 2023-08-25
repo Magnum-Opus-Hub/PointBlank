42f1c2704ccf5f53aad730c59586b6002d38a56e
FilipTheGod <filipstan666@gmail.com>
2023-08-25 20:41:00 +0300

    iphone video 2

--- components/nav/Animation.jsx
+++ components/nav/Animation.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 
 const Animation = () => {
+  const [isVideoPlaying, setVideoPlaying] = useState(false);
+
+  const handlePlayClick = () => {
+    setVideoPlaying(true);
+  };
+
   return (
     <div className="flex items-center justify-center h-screen">
-     <video autoPlay muted loop playsInline className="absolute inset-0 w-full h-full object-cover">
-  <source src='/pbklogo.mp4' type='video/mp4' />
-  Your browser does not support the video tag
-</video>
-
+      {isVideoPlaying ? (
+        <video autoPlay muted loop playsInline className="absolute inset-0 w-full h-full object-cover">
+          <source src='/pbklogo.mp4' type='video/mp4' />
+          Your browser does not support the video tag
+        </video>
+      ) : (
+        <div className="relative">
+          <button onClick={handlePlayClick} className="absolute inset-0 w-full h-full bg-black bg-opacity-50 text-white flex items-center justify-center">
+            Play Video
+          </button>
+        </div>
+      )}
     </div>
   );
 };
