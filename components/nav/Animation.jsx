12abb143c6cfa230ab6c732ed586a90baf6717f3
FilipTheGod <filipstan666@gmail.com>
2023-08-26 19:04:28 +0300

    done preview

--- components/nav/Animation.jsx
+++ components/nav/Animation.jsx
@@ -8,7 +8,7 @@ const Animation = () => {
         muted
         loop
         playsInline
-        className="absolute inset-0 w-full h-full object-cover object-center"
+        className="absolute top-0 left-0 w-full h-auto md:h-full object-contain md:object-cover"
       >
         <source src='/pbklogo.mp4' type='video/mp4' />
         Your browser does not support the video tag
